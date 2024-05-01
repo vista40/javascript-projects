bab6f699c8532a31f68307818fa2686d282f6801
Anne Stahl/Vista 40 <afspiano@yahoo.com>
2024-05-01 16:45:01 -0500

    Stahl Debugging exercises ch. 6

--- errors-and-debugging/exercises/DebuggingLogicErrors5.js
+++ errors-and-debugging/exercises/DebuggingLogicErrors5.js
@@ -17,12 +17,22 @@ if (fuelLevel >= 20000) {
 
 console.log("launchReady = ", launchReady);
 
-if (crewStatus && computerStatus === 'green'){
+if (crewStatus && computerStatus === 'green') {
    console.log('Crew & computer cleared.');
-   launchReady = true;
+   crewReady = true;
 } else {
    console.log('WARNING: Crew or computer not ready!');
-   launchReady = false;
+   crewReady = false;
 }
 
 console.log("launchReady = ", launchReady);
+if (launchReady) {
+   console.log('10, 9, 8, 7, 6, 5, 4, 3, 2, 1...');
+   console.log('Liftoff!');
+} else {
+   console.log('Launch scrubbed.');
+}
+if (launchReady) {
+   console.log('10, 9, 8, 7, 6, 5, 4, 3, 2, 1...');
+   console.log('Liftoff!');
+}
