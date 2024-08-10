a5735afe65150eb6a5ea68c2422c3b0f5208fff0
Anne Stahl/Vista 40 <afspiano@yahoo.com>
2024-08-10 11:14:15 -0500

    StahlCh.14DomExercises

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -1,10 +1,21 @@
-function init () {
-    const missionAbort = document.getElementById("abortMission");
-    const button = document.getElementById("liftoffButton");
-    const paragraph = document.getElementById("statusReport");
+function init() {
+  const missionAbort = document.getElementById("abortMission");
+  const button = document.getElementById("liftoffButton");
+  const paragraph = document.getElementById("statusReport");
 
-    // Put your code for the exercises here.
-    
+  button.addEventListener("click", (event) => {
+    paragraph.innerHTML = "Houston! We have liftoff!";
+  });
+  missionAbort.addEventListener("mouseover", function (event) {
+    event.target.style.backgroundColor = "red";
+  });
+  missionAbort.addEventListener("mouseout", function (event) {
+    event.target.style.backgroundColor = "";
+  });
+  missionAbort.addEventListener("click", function (event) {
+    window.confirm("Are you sure you want to abort the mission?");
+    paragraph.innerHTML = "Mission aborted! Space shuttle returning home";
+  });
 }
 
 window.addEventListener("load", init);
